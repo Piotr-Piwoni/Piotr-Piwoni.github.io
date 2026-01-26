787244390be0c186717933fcf85e42f6db217957
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2026-01-26 11:25:41 +0000

    Updated the "John" Project Page.
    - Added a "Deep-Dive" section to the page talking more about my role and what I worked on.

--- dist/variables.js
+++ dist/variables.js
@@ -1,4 +1,7 @@
 export const websiteName = "Tusk Games";
 export const viewableProjects = [
     "John",
+    /*	"test1",
+     "test2",
+     "test3",*/
 ];
--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,7 @@
 ﻿export const websiteName: string = "Tusk Games";
 export const viewableProjects: string[] = [
 	"John",
-
+	/*	"test1",
+	 "test2",
+	 "test3",*/
 ];
